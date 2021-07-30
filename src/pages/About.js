b0091fce9fddb8c9f90597eb4ec2dc1c1681359f
Elcin Koyuncu <79725177+ElcinKoyuncu@users.noreply.github.com>
2021-07-30 08:58:43 -0500

    updating files

---
+++ src/pages/About.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import { Container, Row, Col } from '../components/Grid';
+import Jumbotron from '../components/Jumbotron';
+import picture from '../assets/profile_picture.png';
+
+const About = () => {
+   return (
+      <Container>
+         <Jumbotron className="mt-3 mb-3">
+            <h1 className="bigWordName m-0 text-right">Elcin Koyuncu</h1>
+            <h1 className="bigWord m-0 text-right">Full Stack Developer</h1> 
+         </Jumbotron>
+         <Row fluid>
+            <Col size="md-4">   
+               <img src={picture} className="img-thumbnail mt-3" alt="eunah-headshot" />
+            </Col>
+            <Col size="md-8">   
+               <h2 className="mb-1 text-info">who am i?</h2>
+               <p>Solution-driven Full Stack Web Developer with 8 years of analytical experience; proven management, 
+sales and coaching skills. Recognized for innovation, being detail oriented, working under pressure 
+and adapting new conditions. Excellent interpersonal communication skills and collaborator who 
+works well on team-based projects as well as independently. Recognized for proven complex 
+problem-solving abilities to contribute effectively as a part of a fast-paced, quality-driven team.</p>
+               <h2 className="mb-1 text-info">skills</h2>
+               <p>HTML5 | CSS3 | JAVASCRIPT | jQUERY | NODE.JS | MONGO-DB | MYSQL | REACT | EXPRESS | EXPRESS-HANDLEBARS | JSON | ADOBE ILLUSTRATOR | ADOBE PHOTOSHOP | ADOBE INDESIGN | ADOBE DREAMWEAVER</p>    
+               <h2 className="mb-1 text-info">education</h2> 
+               <h4 className="mb-0">UNIVERSITY OF MIAMI</h4>
+               <span className="text-white ml-3">MARCH - AUGUST 2021</span><br/>
+               <span className="text-white ml-3">FULL STACK WEB DEVELOPMENT CERTIFICATE</span>
+               <h4 className="mb-0">CUKUROVA UNIVERSITY</h4>
+               <small className="text-white ml-3">BS DEGREE IN PHYSICS</small><br/>
+               <span className="text-white ml-3">2006</span>
+            </Col>
+         </Row>
+      </Container>
+   ) 
+};
+
+export default About;
