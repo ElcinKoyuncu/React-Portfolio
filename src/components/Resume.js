2a299e16c721b0d7d4ab7c7b66b4a0ade23e86ff
Elcin Koyuncu <79725177+ElcinKoyuncu@users.noreply.github.com>
2021-08-07 16:54:49 -0500

    fixing resume

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -8,7 +8,7 @@ const AboutMe = () => {
     <MDBContainer className="resume mt-5">
       <MDBRow>
         <MDBCol md="12">
-          <embed id="Resume" src={Resume} width="100%" height="750px" backgroundColor="white"/>
+          <embed id="Resume" src={Resume} width="100%" height="750px" />
         </MDBCol>
       </MDBRow>
     </MDBContainer>
